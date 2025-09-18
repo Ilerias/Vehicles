d63c34cb9cb1a22ff0cba439e0e95a73d0917c14
Ilerias <johni92.dell@gmail.com>
2025-09-18 22:58:14 +0200

    truck osztály hozzáadása

---
+++ Vehicles/Car.cs
@@ -0,0 +1,16 @@
+namespace Vehicles;
+
+public class Car
+{
+    public string Brand { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public int HorsePower { get; set; }
+    public int Torque { get; set; }
+    public int Fuel { get; set; }
+    public int Year { get; set; }
+    public string LicensePlateNumber { get; set; }
+    public int Price { get; set; }
+}
+
+
---
+++ Vehicles/Truck.cs
@@ -0,0 +1,14 @@
+namespace Vehicles;
+
+public class Truck
+{
+    public string Brand { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public int HorsePower { get; set; }
+    public int Torque { get; set; }
+    public int PayloadCapacity { get; set; }
+    public int NumberOfAxles { get; set; }
+    public string LicensePlateNumber { get; set; }
+    public int Price { get; set; }
+}
